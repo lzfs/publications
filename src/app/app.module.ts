b4897998ce2ea759867023c675075415de6dfc78
Laurenz Fuchs <laurenz.fuchs@gmail.com>
2021-05-14 11:49:33 +0200

    setup components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PublicationComponent } from './publication/publication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    DashboardComponent,
+    PublicationComponent
   ],
   imports: [
     BrowserModule
