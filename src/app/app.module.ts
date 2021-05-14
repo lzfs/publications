e89e96722efb3d03a786b55e60d9722f16e28b50
Laurenz Fuchs <laurenz.fuchs@gmail.com>
2021-05-14 12:21:24 +0200

    add: basic component and service functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PublicationComponent } from './publication/publication.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,11 @@ import { PublicationComponent } from './publication/publication.component';
     PublicationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/publication.service.ts
+++ src/app/shared/publication.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Publication } from './publication';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicationService {
 
-  constructor() { }
+  private api = 'https://athene-forschung.rz.unibw-muenchen.de/services/export/node/92555';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Publication[]> {
+    return this.http.get<Publication[]>(`${this.api}/allchildren`);
+  }
 }
